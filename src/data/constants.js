8af9a601c18127f2228468ba7e747b4a001b98cb
MuhdTaha <124110272+MuhdTaha@users.noreply.github.com>
2024-02-05 15:03:51 -0600

    Website launch

---
+++ src/data/constants.js
@@ -0,0 +1,180 @@
+export const Bio = {
+    name: "Muhammad Taha",
+    roles: [
+      "CS Student",
+      "Developer",
+      "Programmer",
+    ],
+    description:
+      "As a driven and adaptable Computer Science student, I thrive on embracing new challenges. My insatiable passion for learning is the driving force behind my commitment to delivering top-notch results. Equipped with a positive attitude and a growth mindset, I am poised to contribute meaningfully and achieve remarkable milestones.",
+    github: "https://github.com/MuhdTaha",
+    resume:
+      "https://drive.google.com/file/d/1Yk8AhrtuXg6pXmd1KWaQzR6wBXYtnxxX/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/muhdtaha",
+    twitter: "",
+    insta: "",
+    facebook: "",
+  };
+  
+  export const skills = [
+    {
+      title: "Languages",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "C",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/1853px-C_Programming_Language.svg.png",
+        },
+        {
+          name: "C++",
+          image:
+            "https://w7.pngwing.com/pngs/46/626/png-transparent-c-logo-the-c-programming-language-computer-icons-computer-programming-source-code-programming-miscellaneous-template-blue.png",
+        },
+        {
+          name: "Kotlin",
+          image:
+            "https://download.logo.wine/logo/Kotlin_(programming_language)/Kotlin_(programming_language)-Logo.wine.png",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+      ],
+    },
+    {
+      title: "Tools",
+      skills: [
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://1000logos.net/wp-content/uploads/2020/08/MySQL-Logo.png",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://w7.pngwing.com/pngs/956/695/png-transparent-mongodb-original-wordmark-logo-icon-thumbnail.png",
+        },
+      ],
+    },
+    {
+      title: "Frameworks",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "React Js",
+          image:
+            "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png",
+        },
+        {
+          name: "Spring",
+          image:
+            "https://dz2cdn1.dzone.com/storage/temp/12434118-spring-boot-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Softwares",
+      skills: [
+        {
+          name: "IntelliJ IDEA",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/IntelliJ_IDEA_Icon.svg/2048px-IntelliJ_IDEA_Icon.svg.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "CLion",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Clion.svg/2500px-Clion.svg.png",
+        },
+        {
+          name: "Android Studios",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Android_Studio_icon_%282023%29.svg/2048px-Android_Studio_icon_%282023%29.svg.png",
+        },
+      ],
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Brain Busters Quiz",
+      date: "Dec 2023",
+      description:
+        "The Brain Busters quiz is a Java Swing application that allows users to participate in a quiz with questions from three different categories: Sports, Geography, and General Knowledge. The user interface has been designed with a welcoming screen presenting category options, a question display with radio buttons for answers, and a final screen showing the total score.",
+      image:
+        "https://raw.githubusercontent.com/MuhdTaha/QuizApp/main/QuizApp%20Screenshot.png",
+      tags: ["Java", "Java Swing", "Java AWT", "MySQL", "JDBC"],
+      github: "https://github.com/MuhdTaha/QuizApp",
+      webapp: "https://drive.google.com/file/d/1DbXsDiCLC-JQyZ6ZYxFbsiRaAaQOesRK/view?usp=sharing",
+    },
+    {
+      id: 1,
+      title: "File Management System",
+      date: "",
+      description:
+        "A Java web application that provides a file management system for employees to access, download, and manage files. ",
+      image:
+        "https://raw.githubusercontent.com/MuhdTaha/FileManagement/main/Login%20Page%20Screenshot.png",
+      tags: [
+        "Java",
+        "JSP",
+        "HTML",
+        "Apache Tomcat",
+        "MySQL",
+      ],
+      github: "https://github.com/MuhdTaha/FileManagement",
+      webapp: "https://drive.google.com/file/d/1z2dl0MBdT5VT4_4a1xC047Z1SaxEMAf9/view?usp=sharing",
+    },
+    {
+      id: 3,
+      title: "Dictionary Android App in Kotlin",
+      date: "Jan 2024",
+      description:
+        "This dictionary app uses the Free Dictionary API to allows users to search for the meanings of words, synonyms, antonyms, including phonetics and multiple meanings, displayed in a user-friendly interface.",
+      image:
+        "https://raw.githubusercontent.com/MuhdTaha/DictionaryApp/main/DictionaryAppScreenshot.png",
+      tags: [
+        "Kotlin",
+        "Java",
+        "XML",
+        "API",
+        "Android Studios",
+      ],
+      github: "https://github.com/MuhdTaha/DictionaryApp",
+      webapp: "https://drive.google.com/file/d/14TjnDF6HWdfaA20Ecum43Acog3qY8ibk/view?usp=sharing",
+    },
+  ];
+  
---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg: "#271c1d",
+    bgLight: "#2b0105",
+    primary: "#a71930",
+    text_primary: "#F2F3F4",
+    text_secondary: "#b1b2b3",
+    card: "#1a0204",
+    card_light: "#1a0204",
+    button: "#e32d2d",
+    white: "#FFFFFF",
+    black: "#000000",
+  };
+  
+  export const lightTheme = {
+    bg: "#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary: "#be1adb",
+    text_primary: "#111111",
+    text_secondary: "#48494a",
+    card: "#FFFFFF",
+    button: "#5c5b5b",
+  };
